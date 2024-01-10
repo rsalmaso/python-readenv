711de505ceb0f9f0322ae677d6cf113847a8e666
Raffaele Salmaso <raffaele@salmaso.org>
2024-01-10 21:44:51 +0100

    Added nox lint rules

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,26 @@ from typing import Final, List
 
 import nox
 
+FILES: Final[List[str]] = ["readenv", "noxfile.py"]
 PYTHON: Final[List[str]] = ["3.8", "3.9", "3.10", "3.11", "3.12"]
+
+nox.options.sessions = ["lint"]
 
 
 def requirements_file(session: nox.Session) -> str:
     return f"requirements-{session.python}.txt"
+
+
+def install(session: nox.Session) -> None:
+    with session.cd("requirements"):
+        session.install("-r", requirements_file(session))
+
+
+@nox.session(python=["3.10"])
+def lint(session: nox.Session) -> None:
+    install(session)
+    session.run("ruff", *FILES)
+    session.run("mypy", *FILES)
 
 
 @nox.session(python=PYTHON)
